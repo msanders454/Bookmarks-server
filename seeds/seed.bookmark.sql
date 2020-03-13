dd0a6dc7e969ca4c5b2c8b2466a01fb70b07a800
Michael Sanders <mikesanderspsu@gmail.com>
2020-03-13 19:13:48 -0400

    Finishing test info

---
+++ seeds/seed.bookmark.sql
@@ -0,0 +1,5 @@
+INSERT INTO bookmarks (title, url, rating, description)
+VALUES
+  ('Thinkful', 'https://www.thinkful.com', '5', 'Content 1'),
+  ('Google', 'https://www.google.com', '4', 'Content 2'),
+  ('MDN', 'https://developer.mozilla.org', '5', 'content 3');
