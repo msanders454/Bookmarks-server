6a579e2c9e3d1111a3a592268c6d18a52507b63a
Michael Sanders <mikesanderspsu@gmail.com>
2020-03-14 15:50:17 -0400

    Finishing test info

--- src/app.js
+++ src/app.js
@@ -4,44 +4,30 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const logger = require('./logger')
 const app = express()
 const bookmarkRouter = require('./bookmarks/bookmarks-router');
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
+app.use(cors())
 app.use(helmet())
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
-  logger.error(`Unauthorized request to path: ${req.path}`);
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-    return res.status(401).json({ error: 'Unauthorized request' })
-  }
-  // move to the next middleware
-  next()
-})
-
-app.use(bookmarkRouter);
+app.use(bookmarkRouter)
 
 app.get('/', (req, res) => {
- res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
- let response
- if (NODE_ENV === 'production') {
- response = { error: { message: 'server error' } }
- } else {
- console.error(error)
- response = { message: error.message, error }
- }
- res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
 })
 
 module.exports = app
-
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -1,94 +1,96 @@
-require('dotenv').config();
-
-const express = require('express');
-const uuid = require('uuid/v4'); //generate uuids for new bookmark ids
-const logger = require('../logger');
-const store = require('../store')
+const express = require('express')
+const uuid = require('uuid/v4')
 const { isWebUri } = require('valid-url')
-const bookmarkRouter = express.Router(); 
-const bodyParser = express.json(); //to parse json in POST endpoint
-const { bookmarks } = require('../store')
+const logger = require('../logger')
+const store = require('../store')
+const BookmarksService = require('./bookmarks-service')
 
-bookmarkRouter
- .route('/bookmark')
- .get((req, res, next) => {
-    res.json(bookmarks)
+const bookmarksRouter = express.Router()
+const bodyParser = express.json()
+
+const serializeBookmark = bookmark => ({
+  id: bookmark.id,
+  title: bookmark.title,
+  url: bookmark.url,
+  description: bookmark.description,
+  rating: Number(bookmark.rating),
+})
+
+bookmarksRouter
+  .route('/bookmarks')
+  .get((req, res, next) => {
+    BookmarksService.getAllBookmarks(req.app.get('db'))
+      .then(bookmarks => {
+        res.json(bookmarks.map(serializeBookmark))
+      })
+      .catch(next)
   })
-  
- .post(bodyParser, (req, res) => {
+  .post(bodyParser, (req, res) => {
+    // TODO: update to use db
     for (const field of ['title', 'url', 'rating']) {
-        if (!req.body[field]) {
-          logger.error(`${field} is required`)
-          return res
-          .status(400)
-          .send(`'${field}' is required`)
-        }
+      if (!req.body[field]) {
+        logger.error(`${field} is required`)
+        return res.status(400).send(`'${field}' is required`)
       }
-      const { title, url, description, rating } = req.body
-  
-      if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
-        logger.error(`Invalid rating '${rating}`)
-        return res
-        .status(400)
-        .send(`'Must be a number between 0 and 5`)
-      }
-  
-      if (!isWebUri(url)) {
-        logger.error(`Invalid url '${url}`)
-        return res
-        .status(400)
-        .send(`'Must be a valid URL`)
-      }
-  
-      const bookmark = { 
-        id: uuid(),
-        title, 
-        url, 
-        description, 
-        rating 
-      }
-  
-      bookmarks.push(bookmark)
-  
-      logger.info(`Bookmark with id ${bookmark.id} created`)
-      res
-        .status(201)
-        .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
-        .json(bookmark)
-    });
+    }
+    const { title, url, description, rating } = req.body
 
-bookmarkRouter
- .route('/bookmark/:id')
- .get((req, res) => {
-    const {id}  = req.params
-    const bookmark = bookmarks.find(b => b.id == id);
-    if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
-      return res
-      .status(404)
-      .send('Bookmark Not Found');
+    if (!Number.isInteger(rating) || rating < 0 || rating > 5) {
+      logger.error(`Invalid rating '${rating}' supplied`)
+      return res.status(400).send(`'rating' must be a number between 0 and 5`)
     }
 
-    res.json(bookmark)
+    if (!isWebUri(url)) {
+      logger.error(`Invalid url '${url}' supplied`)
+      return res.status(400).send(`'url' must be a valid URL`)
+    }
+
+    const bookmark = { id: uuid(), title, url, description, rating }
+
+    store.bookmarks.push(bookmark)
+
+    logger.info(`Bookmark with id ${bookmark.id} created`)
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${bookmark.id}`)
+      .json(bookmark)
   })
-.delete((req, res, next) => {
-    
-    const { id  } = req.params;
-    const bookmarkIndex = bookmarks.findIndex(b => b.id === id);
+
+bookmarksRouter
+  .route('/bookmarks/:bookmark_id')
+  .get((req, res, next) => {
+    const { bookmark_id } = req.params
+    BookmarksService.getById(req.app.get('db'), bookmark_id)
+      .then(bookmark => {
+        if (!bookmark) {
+          logger.error(`Bookmark with id ${bookmark_id} not found.`)
+          return res.status(404).json({
+            error: { message: `Bookmark Not Found` }
+          })
+        }
+        res.json(serializeBookmark(bookmark))
+      })
+      .catch(next)
+  })
+  .delete((req, res) => {
+    // TODO: update to use db
+    const { bookmark_id } = req.params
+
+    const bookmarkIndex = store.bookmarks.findIndex(b => b.id === bookmark_id)
 
     if (bookmarkIndex === -1) {
-      logger.error(`Bookmark with id ${id} not found.`);
+      logger.error(`Bookmark with id ${bookmark_id} not found.`)
       return res
         .status(404)
-        .send('Bookmark Not Found');
+        .send('Bookmark Not Found')
     }
 
-    bookmarks.splice(bookmarkIndex, 1)
+    store.bookmarks.splice(bookmarkIndex, 1)
 
-    logger.info(`Bookmark with id ${id} deleted.`)
+    logger.info(`Bookmark with id ${bookmark_id} deleted.`)
     res
       .status(204)
       .end()
   })
 
- module.exports = bookmarkRouter;
+module.exports = bookmarksRouter
